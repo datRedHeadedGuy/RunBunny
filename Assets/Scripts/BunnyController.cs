2e14c5dd097b41c60e1cc574d1fa9f6d5b9d35d4
datRedHeadedGuy <jwintermeyer@ymail.com>
2016-10-20 00:13:11 -0500

    Added bunny animations

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class BunnyController : MonoBehaviour {
 
     private Rigidbody2D myRigidBody;
+    private Animator myAnimation;
     public float bunnyJumpForce = 500f;
 
 	// Use this for initialization
 	void Start () {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimation = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -17,5 +19,8 @@ public class BunnyController : MonoBehaviour {
         {
             myRigidBody.AddForce(transform.up * bunnyJumpForce);
         }
-	}
+
+        myAnimation.SetFloat("vVelocity", myRigidBody.velocity.y);
+        // myAnimation.SetFloat("vVelocity", Mathf.Abs(myRigidBody.velocity.y));
+    }
 }

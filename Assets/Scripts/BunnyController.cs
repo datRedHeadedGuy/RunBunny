c944ce7a0b62290e46c035f2dd238fb9cc687e5f
datRedHeadedGuy <jwintermeyer@ymail.com>
2016-10-20 02:11:10 -0500

    Added Bunny death and scoring

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -1,34 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BunnyController : MonoBehaviour {
 
     private Rigidbody2D myRigidBody;
     private Animator myAnimation;
+    private float bunnyHurtTime = -1;
+    private Collider2D myCollider;
+    private float startTime;
     public float bunnyJumpForce = 500f;
+    public Text scoreText;
 
 	// Use this for initialization
 	void Start () {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimation = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+        startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if ( Input.GetButtonUp("Jump") )
+        if (bunnyHurtTime == -1)
         {
-            myRigidBody.AddForce(transform.up * bunnyJumpForce);
-        }
+            if (Input.GetButtonUp("Jump"))
+            {
+                myRigidBody.AddForce(transform.up * bunnyJumpForce);
+            }
 
-        myAnimation.SetFloat("vVelocity", myRigidBody.velocity.y);
-        // myAnimation.SetFloat("vVelocity", Mathf.Abs(myRigidBody.velocity.y));
+            myAnimation.SetFloat("vVelocity", myRigidBody.velocity.y);
+            // myAnimation.SetFloat("vVelocity", Mathf.Abs(myRigidBody.velocity.y));
+
+            scoreText.text = (Time.time - startTime).ToString("0.0");
+        }
+        else
+        {
+            if (Time.time > bunnyHurtTime + 2)
+            {
+                // Application.LoadLevel(Application.loadedLevel);
+                SceneManager.LoadScene("Game");
+            }
+        }
     }
 
     // 
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            Application.LoadLevel(Application.loadedLevel);
+            foreach (PrefabSpawner spawner in FindObjectsOfType<PrefabSpawner>())
+            {
+                spawner.enabled = false;
+            }
+
+            foreach (MoveLeft lefter in FindObjectsOfType<MoveLeft>())
+            {
+                lefter.enabled = false;
+            }
+
+            bunnyHurtTime = Time.time;
+            myAnimation.SetBool("bHurt", true);
+            myRigidBody.velocity = Vector2.zero;
+            myRigidBody.AddForce(transform.up * bunnyJumpForce);
+            myCollider.enabled = false;
+            // Application.LoadLevel(Application.loadedLevel);
+            // SceneManager.LoadScene("Game");
         }
     }
 }

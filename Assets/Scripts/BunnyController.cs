afaf690b78da83a5d3fdad02aecd2b9382419aa7
datRedHeadedGuy <jwintermeyer@ymail.com>
2016-10-21 21:22:39 -0500

    Added a title screen

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -26,6 +26,12 @@ public class BunnyController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Application.LoadLevel("Title");
+            SceneManager.LoadScene("Title");
+        }
+
         if (bunnyHurtTime == -1)
         {
             if (Input.GetButtonUp("Jump") && jumpsLeft > 0)
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void StartGame() {
+        // Application.LoadLevel("Game");
+        SceneManager.LoadScene("Game");
+    }
+}

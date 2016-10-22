fb03e3b6801235af012fbd11549d511d4a992b1f
datRedHeadedGuy <jwintermeyer@ymail.com>
2016-10-22 04:14:13 -0500

    Tweaks for mobile devices

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -4,10 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour {
 
+    public GameObject audioOnIcon;
+    public GameObject audioOffIcon;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        SetSoundState();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,5 +23,35 @@ public class MenuController : MonoBehaviour {
     public void StartGame() {
         // Application.LoadLevel("Game");
         SceneManager.LoadScene("Game");
+    }
+
+    public void ToggleSound()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            PlayerPrefs.SetInt("Muted", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("Muted", 0);
+        }
+
+        SetSoundState();
+    }
+
+    private void SetSoundState()
+    {
+        if (PlayerPrefs.GetInt("Muted", 0) == 0)
+        {
+            AudioListener.volume = 1;
+            audioOnIcon.SetActive(true);
+            audioOffIcon.SetActive(false);
+        }
+        else
+        {
+            AudioListener.volume = 0;
+            audioOnIcon.SetActive(false);
+            audioOffIcon.SetActive(true);
+        }
     }
 }
